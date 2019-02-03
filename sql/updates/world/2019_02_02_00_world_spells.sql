acb14727c7d4926c86d5e9abea8e953ad6d95bfc
Eliminationzx <timurdelimin@gmail.com>
2019-02-04 01:08:54 +0700

    Core/Spells: * Fix class mount visual effects * Fix warrior spells Shouts * Fix spell monk Transcendence

--- sql/updates/world/2019_02_02_00_world_spells.sql
+++ sql/updates/world/2019_02_02_00_world_spells.sql
@@ -1,2 +1,23 @@
 DELETE FROM `spell_script_names` WHERE `spell_id`=108212 AND `ScriptName`='spell_rog_burst_of_speed';
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES (108212, 'spell_rog_burst_of_speed');
+
+-- Transcendence
+UPDATE creature_template SET ScriptName = 'npc_transcendence_spirit' WHERE entry = 54569;
+DELETE FROM spell_script_names WHERE ScriptName = 'spell_monk_transcendence_transfer';
+INSERT INTO spell_script_names VALUE (119996, 'spell_monk_transcendence_transfer');
+
+-- Warrior Shouts
+DELETE FROM spell_script_names WHERE ScriptName = 'spell_warr_shouts';
+INSERT INTO spell_script_names VALUE 
+(469, 'spell_warr_shouts'),
+(6673, 'spell_warr_shouts');
+
+-- Mount summon visual effects
+DELETE FROM spell_linked_spell WHERE spell_trigger IN (5784, 13819, 23161, 23214, 34767, 34769, 48778);
+INSERT INTO `spell_linked_spell` VALUES ('5784', '31725', '0', 'Summon felsteed');
+INSERT INTO `spell_linked_spell` VALUES ('13819', '31726', '0', 'Summon Warhorse');
+INSERT INTO `spell_linked_spell` VALUES ('23161', '31725', '0', 'Summon Nightmare');
+INSERT INTO `spell_linked_spell` VALUES ('23214', '31726', '0', 'Summon Charger');
+INSERT INTO `spell_linked_spell` VALUES ('34767', '31726', '0', 'Summon Charger - Blood Elf');
+INSERT INTO `spell_linked_spell` VALUES ('34769', '31726', '0', 'Summon Warhorse - Blood Elf');
+INSERT INTO `spell_linked_spell` VALUES ('48778', '50772', '0', 'Acherus Deathcharger - Summon Unholy Mount Visual');
