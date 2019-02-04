c5dc17290ac01c09c78c090ea208380f1fcf808b
Rooter <Slipknot500.slipknot@yandex.ru>
2019-02-04 11:04:42 +0300

    Db/GameObject: Добавить лут объекту 221672

--- sql/updates/world/2019_04_02_00_world_game_object_loot_fix.sql
+++ sql/updates/world/2019_04_02_00_world_game_object_loot_fix.sql
@@ -1,4 +1,5 @@
 -- fix loot for gameobject
 UPDATE `gameobject_template` SET `data1`='223204' WHERE (`entry`='223117');
 UPDATE `gameobject_template` SET `data1`='223204' WHERE (`entry`='223108');
+UPDATE `gameobject_template` SET `data1`='223204' WHERE (`entry`='221672');
 
