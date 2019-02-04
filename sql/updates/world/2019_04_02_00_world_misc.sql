5ed8bf8909c152ab1677a5a9b2a4409c79af5d8e
Eliminationzx <timurdelimin@gmail.com>
2019-02-04 10:12:12 +0700

    Fix go rotation

--- sql/updates/world/2019_04_02_00_world_misc.sql
+++ sql/updates/world/2019_04_02_00_world_misc.sql
@@ -1,6 +1,8 @@
 -- Fix Stove rotation
 DELETE FROM gameobject WHERE guid=527460 AND id=222792;
 INSERT INTO `gameobject` VALUES ('527460', '222792', '870', '0', '0', '1', '1', '-787.753', '-5087.27', '2.05181', '3,56178', '0', '0', '0,978011', '-0,208552', '300', '255', '1', '0', '55');
+DELETE FROM gameobject WHERE guid=527094 AND id=222793;
+INSERT INTO `gameobject` VALUES ('527094', '222793', '870', '0', '0', '1', '1', '-614.774', '-4911.24', '2.05349', '1.37008', '0', '0', '0', '1', '120', '255', '1', '0', '57');
 
 -- Immolation for Abyssal and Infernal
 UPDATE creature_template_addon SET auras='19483' WHERE entry IN (89, 58997);
