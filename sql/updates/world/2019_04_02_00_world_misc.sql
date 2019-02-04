bde5cdb7dbf67481986cba26aaa092d09f818e57
Rooter <Slipknot500.slipknot@yandex.ru>
2019-02-04 07:06:01 +0300

    Db/Creature: Поправить нпс 59000

--- sql/updates/world/2019_04_02_00_world_misc.sql
+++ sql/updates/world/2019_04_02_00_world_misc.sql
@@ -6,3 +6,7 @@ INSERT INTO `gameobject` VALUES ('527094', '222793', '870', '0', '0', '1', '1', 
 
 -- Immolation for Abyssal and Infernal
 UPDATE creature_template_addon SET auras='19483' WHERE entry IN (89, 58997);
+
+-- Fix npc 59000
+UPDATE `creature_template` SET `AIName`='SmartAI' WHERE (`entry`='59000');
+INSERT INTO `smart_scripts` (`entryorguid`, `source_type`, `id`, `link`, `event_type`, `event_phase_mask`, `event_chance`, `event_flags`, `event_param1`, `event_param2`, `event_param3`, `event_param4`, `action_type`, `action_param1`, `action_param2`, `action_param3`, `action_param4`, `action_param5`, `action_param6`, `target_type`, `target_param1`, `target_param2`, `target_param3`, `target_x`, `target_y`, `target_z`, `target_o`, `comment`) VALUES ('59000', '0', '0', '0', '0', '0', '100', '0', '500', '1000', '2500', '3000', '11', '85692', '0', '0', '0', '0', '0', '2', '0', '0', '0', '0', '0', '0', '0', '');
